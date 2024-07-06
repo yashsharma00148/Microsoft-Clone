9842f5dbdd444bdeebef57aef5340599dd8c2111
Yash <yashsharma786a@gmail.com>
2024-07-06 14:31:43 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,146 @@
+let slides=document.querySelectorAll(".slide")
+let left=document.querySelector(".left")
+let next=document.querySelector(".next")
+let counter=0
+console.log(slides)
+let circle1=document.querySelector("#circle1")
+let circle2=document.querySelector("#circle2")
+let section2=document.querySelector(".section2")
+let section4=document.querySelector(".section4")
+
+
+let list=document.querySelector(".responsive-list")
+let hamburger=document.querySelector("#menu")
+menu.addEventListener("click",()=>{
+    if(menu.classList.contains("fa-bars")){
+        menu.classList.add("rotate")
+       setTimeout(() => {
+        menu.classList.remove("rotate")
+        
+       }, 400);
+        menu.classList.remove("fa-bars")
+        menu.classList.add("fa-x")
+        list.style.left="0px"
+        
+    }
+    else{
+        menu.classList.add("rotate")
+        setTimeout(() => {
+         menu.classList.remove("rotate")
+         
+        }, 400);
+        menu.classList.add("rotate")
+        
+        menu.classList.add("fa-bars")
+        menu.classList.remove("fa-x")
+        list.style.left="-200vw"
+
+    }
+})
+
+
+
+
+
+
+
+
+
+
+
+const updateimage=()=>{
+    
+    slides.forEach((slide,index)=>{
+        
+            slide.style.transform=`translateX(-${(index-counter)*100}%)`
+
+        
+            console.log("index:",index)
+        
+    })
+}
+next.addEventListener("click",()=>{
+    counter++
+    console.log(counter)
+    if(counter>=slides.length){
+        counter=0
+    }
+    updateimage()
+})
+
+left.addEventListener("click",()=>{
+    counter--
+    console.log(counter)
+    if(counter<0){
+        counter=slides.length-1
+    }
+    updateimage()
+})
+
+
+// const autoslider=()=>{
+//     setInterval(() => {
+//         counter++
+//         if(counter>=slides.length){
+//             counter=0;
+//             updateimage()
+        
+//         }
+        
+//     }, 2000);
+//     }
+
+
+
+
+// autoslider()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+boxes.forEach((box,index)=>{
+    let content=document.createElement("div")
+    content.classList.add("box")
+    content.innerHTML=` 
+            <img src="${box.img}" alt="">
+            <div class="title"><h1>${box.title}</h1></div>
+            <div class="description"><p>${box.description}</p></div>
+            <button>${box.button}</button>
+        </>`
+        section2.appendChild(content)
+        content.classList.add(`box${index}`)
+
+})
+
+
+
+boxes2.forEach((box2,index)=>{
+    let content=document.createElement("div")
+    content.classList.add("box")
+    content.innerHTML=` 
+            <img src="${box2.img}" alt="">
+            <div class="title"><h1>${box2.title}</h1></div>
+            <div class="description"><p>${box2.description}</p></div>
+            <button>${box2.button}</button>
+        </>`
+        section4.appendChild(content)
+        content.classList.add(`box2${index}`)
+
+})
+
+
+
+
+
+
+
---
+++ section2.js
@@ -0,0 +1,72 @@
+let boxes=[
+    {
+        img:"box1-img.avif",
+        title:"Unlock your potential with Microsoft Copilot",
+        description:"Get things done faster and unleash your creativity with the power of AI anywhere you go.",
+        button:"Download the Copilot App"
+        
+    },
+    {
+        img:"box2-img.avif",
+        title:"Maximise the everyday with Microsoft 365",
+        description:"Get online protection, secure cloud storage and innovative apps designed to fit your needs – all in one plan.",
+        button:"For One Person"
+        
+    },
+    {
+        img:"box3-img.webp",
+        title:"Xbox Series X",
+        description:"The fastest, most powerful Xbox ever.",
+        button:"Shop X-box Series-X"
+        
+    },
+    {
+        img:"box4-img.webp",
+        title:"Xbox Series S",
+        
+        description:"Next-gen performance in the smallest Xbox ever.",
+        button:"Shop X-box Series-S"
+        
+    },]
+
+
+    let boxes2=[
+        {
+            img:"box5.avif",
+            title:"Surface for Business PCs",
+            description:"Boost productivity with the latest Intel processors and AI-powered tools in the Surface Pro 10 for Business and Surface Laptop 6 for Business.",
+            button:"Shop Surface 10 Pro for Business"
+            
+        },
+        {
+            img:"box6.webp",
+            title:"Boost productivity with the Intel Processors.",
+            description:"Securely stream your Windows experience from the Microsoft cloud to any device.",
+            button:"Get it today"
+            
+        },
+        {
+            img:"box7.jpg",
+            title:"Join the era of AI",
+            description:"Create, communicate, and code with the latest Microsoft AI solutions.",
+            button:"Explore AI Solutions"
+            
+        },
+        {
+            img:"box8.avif",
+            title:"Get Microsoft Teams for your business",
+            
+            description:"Online meetings, chat, real-time collaboration, and shared cloud storage—all in one place.",
+            button:"Find the right plan for your business"
+            
+        },
+    ]
+
+
+
+
+
+
+
+
+
